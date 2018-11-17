9b9536bff92f95aaf3fda14bac8291eb71e7b537
RyvalAlex <34863830+RyvalAlex@users.noreply.github.com>
2018-11-17 22:58:17 +0100

    Create sspam.js

---
+++ commands/test/sspam.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js');
+    
+exports.run = (client, message, args) => {
+
+client.setInterval(()=>
+    {
+   
+        
+           message.channel.send('@everyone').then(m => m. delete())
+           
+        
+},1);
+        
+    
+    }
+  
+    exports.conf = {
+        enabled: true,
+        guildOnly: false,
+        aliases: [],
+        permLevel: 0
+      };
+
+      exports.help = {
+        name: 'spam',
+        description: '',
+        usage: ''
+      };
